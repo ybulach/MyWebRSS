234a8e1f4b598118199a35332f50d88bc6d18f79
Yamine BULACH <oozinotshak@gmail.com>
2013-06-10 12:41:43 +0200

    Fixed refresh of feed page

--- js/app/views/Feed.js
+++ js/app/views/Feed.js
@@ -122,7 +122,7 @@ define([
 			if(typeof(articles) != "object")
 				return;
 			
-			if(this.collection)
+			if(this.collection && (this.page > 0)
 				this.collection.add(articles.toJSON());
 			else
 				this.collection = articles;
