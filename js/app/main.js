b51ad514457f75e77d3907ae0fdbf0dce06d6a36
Yamine BULACH <oozinotshak@gmail.com>
2013-08-17 19:42:57 +0200

    Fixed wrong Persona implementation

--- js/app/main.js
+++ js/app/main.js
@@ -27,7 +27,7 @@ define([
 				$.ajax({
 					dataType: "json",
 					url: window.mywebrss + "/user/login",
-					data: {token: assertion},
+					data: {assertion: assertion},
 					success: function(data) {
 						// Check error
 						if(!data.success) {
@@ -50,7 +50,7 @@ define([
 						
 						// Add the email and redirect to Home
 						$.localStorage('email', data.email);
-						$.localStorage('token', assertion);
+						$.localStorage('token', data.token);
 						
 						window.location = "#";
 					},
@@ -62,9 +62,36 @@ define([
 				});
 			},
 			onlogout: function() {
-				$.localStorage("token", null);
-				$.localStorage('email', null);
-				window.location = "#";
+				$.ajax({
+					dataType: "json",
+					url: window.mywebrss + "/user/logout",
+					data: {token: $.localStorage('token')},
+					success: function(data) {
+						// Check error
+						if(!data.success) {
+							var status = new StatusView();
+							
+							// Unknown error
+							if(data.error != "token") {
+								status.setMessage(data.error);
+								return;
+							}
+						}
+						
+						// Delete datas we don't need
+						delete data.success, delete data.error;
+						
+						// Delete the token and redirect to Home
+						$.localStorage("token", null);
+						$.localStorage('email', null);
+						
+						window.location = "#";
+					},
+					error: function() {
+						var status = new StatusView();
+						status.setMessage("Can't contact the server");
+					}
+				});
 			},
 			onready: function() {
 				// Create the Router
