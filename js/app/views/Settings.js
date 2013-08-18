440b707a92ce0a9f65e0625536c2c120dc749afd
Yamine BULACH <oozinotshak@gmail.com>
2013-08-18 17:15:23 +0200

    Cleaned Settings page
    Password change no longer in MyWebRSS

--- js/app/views/Settings.js
+++ js/app/views/Settings.js
@@ -33,56 +33,6 @@ define([
 						clearTimeout($.localStorage("autorefresh_cnt")), $.localStorage("autorefresh_cnt", 0);
 				}
 			});
-			
-			// Change password
-			$("#settings-password").click(function() {
-				$("#settings-password").attr("disabled", "disabled");
-				
-				$.ajax({
-					dataType: "json",
-					url: window.mywebrss + "/user/password",
-					data: {token: $.localStorage("token"), old_password: $("#settings-password-old").val(), password: $("#settings-password-new").val(), confirm_password: $("#settings-password-confirm").val()},
-					success: function(data) {
-						// Check error
-						if(!data.success) {
-							var status = new StatusView();
-							
-							// Wrong token
-							if(data.error == "token") {
-								$.localStorage("token", null);
-								window.location = "#login";
-							}
-							else if(data.error == "old_password") {
-								status.setMessage("Bad password");
-								$("#settings-password-old").focus();
-							}
-							else if(data.error == "password") {
-								status.setMessage("Bad new password");
-								$("#settings-password-new").focus();
-							}
-							else if(data.error == "confirm_password") {
-								status.setMessage("New passwords are different");
-								$("#settings-password-confirm").focus();
-							}
-							// Unknown error
-							else
-								status.setMessage(data.error);
-							
-							$("#settings-password").removeAttr("disabled");
-							return;
-						}
-						
-						alert("Password changed with success");
-						window.location = "#settings";
-					},
-					error: function() {
-						var status = new StatusView();
-						status.setMessage("Can't contact the server");
-						
-						$("#settings-password").removeAttr("disabled");
-					}
-				});
-			});
 		}
 	});
 	
